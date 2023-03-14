7c8a405d44e5e53813f0eecd7a6774fa3bdf6ceb
Widle Studio <amanchandani.widlestudio@gmail.com>
2023-03-14 10:46:27 +0530

    Add Total Daily Energy Expenditure (TDEE) in package

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//fitness calculator
 exports.bmi = require('./lib/fitness/bmi')
 exports.body_fat = require('./lib/fitness/body_fat')
 exports.bmr = require('./lib/fitness/bmr')
@@ -9,4 +10,8 @@ exports.lbm = require('./lib/fitness/lbm')
 exports.healthyWeight = require('./lib/fitness/healthyWeight')
 exports.caloriesBurn = require('./lib/fitness/caloriesBurn')
 
+//pregnancy calculator
 exports.pregnancyWeightGain = require('./lib/pregnancy/pregnancyWeightGain')
+
+//other calculator
+exports.tdee = require('./lib/others/tdee')
